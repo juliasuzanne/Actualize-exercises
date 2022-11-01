60ca9dcf133b71eaf17d463d0c32108fb8f40c2a
Julia Grimes <julia.s.grimes@gmail.com>
2022-10-31 22:24:19 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+contact = Contact.new(first_name: "William", last_name: "Yee", email: "billy@test.com", phone_number: "78192380923")
+contact.save
